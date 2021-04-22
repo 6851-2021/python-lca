135b932691d009b4e9090c4fe2da6c75c6a4cd5f
Jonathan Conroy <jonathan.conroy@tufts.edu>
2021-04-22 01:24:49 -0400

    Remove debugging code

--- rmq.py
+++ rmq.py
@@ -23,60 +23,7 @@ def rmq_query(preprocessed_arr, start_index, end_index):
     start_node = index_to_node[start_index]
     end_node = index_to_node[end_index - 1] # Do not include end_index in query
     lca = lca_query(start_node, end_node, preprocessed_tree)
-    # print("LCA:", lca)
-    # print("LCA data:", lca.data)
     return lca.data
-
-def print_tree(root, val="data", left="left", right="right"):
-    def display(root, val=val, left=left, right=right):
-        """Returns list of strings, width, height, and horizontal coordinate of the root."""
-        # No child.
-        if getattr(root, right) is None and getattr(root, left) is None:
-            line = '%s' % getattr(root, val)
-            width = len(line)
-            height = 1
-            middle = width // 2
-            return [line], width, height, middle
-
-        # Only left child.
-        if getattr(root, right) is None:
-            lines, n, p, x = display(getattr(root, left))
-            s = '%s' % getattr(root, val)
-            u = len(s)
-            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
-            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
-            shifted_lines = [line + u * ' ' for line in lines]
-            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
-
-        # Only right child.
-        if getattr(root, left) is None:
-            lines, n, p, x = display(getattr(root, right))
-            s = '%s' % getattr(root, val)
-            u = len(s)
-            first_line = s + x * '_' + (n - x) * ' '
-            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
-            shifted_lines = [u * ' ' + line for line in lines]
-            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
-
-        # Two children.
-        left, n, p, x = display(getattr(root, left))
-        right, m, q, y = display(getattr(root, right))
-        s = '%s' % getattr(root, val)
-        u = len(s)
-        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
-        second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
-        if p < q:
-            left += [n * ' '] * (q - p)
-        elif q < p:
-            right += [m * ' '] * (p - q)
-        zipped_lines = zip(left, right)
-        lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
-        return lines, n + m + u, max(p, q) + 2, n + u // 2
-
-    lines, *_ = display(root, val, left, right)
-    for line in lines:
-        print(line)
-
 
 def test_rmq():
     arr = np.random.randint(0, 100, size = 100)
